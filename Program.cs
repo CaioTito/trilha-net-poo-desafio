149dd98c71c4486f858bb22c2fd9f2de0efe66b4
Caio Tito <caio_tito@hotmail.com>
2023-10-29 21:24:01 -0300

    feat: implementado conceitos em classe e método de testes dos comportamentos na classe program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "999999999", modelo: "N90", iMEI: "11111111111", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "111111111", modelo: "XR", iMEI: "2222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
